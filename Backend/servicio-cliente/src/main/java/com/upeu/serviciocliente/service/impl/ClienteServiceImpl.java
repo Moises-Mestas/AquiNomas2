f7c41f85b81fde998b33c323109c36c495412be9
302xdwill <davidom302@gmail.com>
2025-06-06 23:31:52 -0500

    new cliente

---
+++ Backend/servicio-cliente/src/main/java/com/upeu/serviciocliente/service/impl/ClienteServiceImpl.java
@@ -0,0 +1,52 @@
+package com.upeu.serviciocliente.service.impl;
+
+import com.upeu.serviciocliente.entity.Cliente;
+import com.upeu.serviciocliente.repository.ClienteRepository;
+import com.upeu.serviciocliente.service.ClienteService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class ClienteServiceImpl implements ClienteService {
+
+    private final ClienteRepository clienteRepository;
+
+    @Override
+    public Cliente saveCliente(Cliente cliente) {
+        return clienteRepository.save(cliente);
+    }
+
+    @Override
+    public Cliente updateCliente(Cliente cliente) {
+        return clienteRepository.save(cliente);
+    }
+
+    @Override
+    public void deleteCliente(Integer id) {
+        clienteRepository.deleteById(id);
+    }
+
+    @Override
+    public Optional<Cliente> getClienteById(Integer id) {
+        return clienteRepository.findById(id);
+    }
+
+    @Override
+    public List<Cliente> getAllClientes() {
+        return clienteRepository.findAll();
+    }
+
+    @Override
+    public List<Cliente> findByNombre(String nombre) {
+        return clienteRepository.findByNombreContainingIgnoreCase(nombre);
+    }
+
+    @Override
+    public Optional<Cliente> findByDni(String dni) {
+        return clienteRepository.findByDni(dni);
+    }
+}
