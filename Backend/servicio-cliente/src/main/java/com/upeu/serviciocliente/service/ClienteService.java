d740fabb3a0f1bfaf9a1149c19aaa24fa6b28240
Moises-Mestas <moisesjoaquin51@gmail.com>
2025-06-08 20:54:39 -0500

    implemento cliente

---
+++ Backend/servicio-cliente/src/main/java/com/upeu/serviciocliente/service/ClienteService.java
@@ -0,0 +1,17 @@
+package com.upeu.serviciocliente.service;
+
+import com.upeu.serviciocliente.entity.Cliente;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ClienteService {
+    Cliente saveCliente(Cliente cliente);
+    Cliente updateCliente(Cliente cliente);
+    void deleteCliente(Integer id);
+    Optional<Cliente> getClienteById(Integer id);
+    List<Cliente> getAllClientes();
+    List<Cliente> findByNombre(String nombre);
+    Optional<Cliente> findByDni(String dni);
+    List<Cliente> listarClientesRecientes(int dias);
+}
