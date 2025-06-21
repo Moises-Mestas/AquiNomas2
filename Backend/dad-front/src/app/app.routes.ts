a64c8a63cf8dbfdb33c280406a3adba1638b6d07
Moises-Mestas <moisesjoaquin51@gmail.com>
2025-06-21 13:47:46 -0500

    ya funca el front pero falta que se vea bacan

--- Backend/dad-front/src/app/app.routes.ts
+++ Backend/dad-front/src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
-import { Recetas } from './pages/recetas/recetas';
+import {MenuPage} from './pages/menu.page/menu.page';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: Home },
-  { path: 'recetas', component: Recetas }
+  { path: 'menus', component: MenuPage }
+
 ];
---
+++ Backend/dad-front/src/app/core/resources/menu.model.ts
@@ -0,0 +1,8 @@
+export interface Menu {
+  id?: number;
+  nombre: string;
+  descripcion?: string;
+  precio?: number;
+  tipo?: string;
+  imagen?: string;
+}
