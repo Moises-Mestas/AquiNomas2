c021bd408fbc1a15f24587fa2eb961aa65860406
302xdwill <davidom302@gmail.com>
2025-06-29 22:02:37 -0500

    front2

--- Backend/servicio-reporte/src/main/java/com/upeu/servicioreporte/config/WebConfig.java
+++ Backend/servicio-reporte/src/main/java/com/upeu/servicioreporte/config/WebConfig.java
@@ -2,22 +2,16 @@ package com.upeu.servicioreporte.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.*;
 
 @Configuration
-public class WebConfig {
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE")
-                        .allowedHeaders("*");
-            }
-        };
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // permite todas las rutas
+                .allowedOrigins("http://localhost:4200") // tu frontend
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*");
     }
 }
-
